960aa307a2caeb7d7e7690d2437595b7da081614
tania <tnt.taniatariq@gmail.com>
2026-04-23 23:09:08 +0500

    updated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,33 @@
 const express = require('express');
+const noteModel = require("./models/note.model")
+
 const app = express()
+app.use(express.json())
+
+//  post/notes => Create a note
+
+app.post("/notes", async (req, res) => {
+    const data = req.body
+    await noteModel.create({
+        title: data.title,
+        description: data.description
+    })
+
+    res.status(201).json({
+        message: "Note created"
+
+    })
+})
+
+app.get("/notes",async (req,res)=>{
+    
+    const notes = await noteModel.find()
+
+    res.status(200).json({
+        message:"Notes fetch successfully",
+        notes:notes
+    })
+})
 
 
 
