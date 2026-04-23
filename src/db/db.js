d3bba76417608fe346ee8de4dc0099d05850e8d1
tania <tnt.taniatariq@gmail.com>
2026-04-23 21:19:10 +0500

    update

--- src/db/db.js
+++ src/db/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
 
 async function connectDB(){
-    await mongoose.connect(process.env.MONGO_URI)
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error)
+    }
 }
-
 module.exports = connectDB
