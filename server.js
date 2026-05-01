8657d41b033e3da4edf86e04d56ab747b2f7c3ac
tania <tnt.taniatariq@gmail.com>
2026-05-02 03:11:27 +0500

    Added schema validation screenshots

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = require("./src/app")
 connectDB()
 
 app.listen(3000,()=>{
-    console.log("Server is running on port 5000")
+    console.log("Server is running on port 3000")
 })
--- src/models/note.model.js
+++ src/models/note.model.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 
-
 const noteSchema = new mongoose.Schema({
-    title:String,
-    description:String,
-    
-})
+  title: {
+    type: String,
+    required: [true, "Title is required"],
+    minlength: [3, "Title must be at least 3 characters"]
+  },
+  description: {
+    type: String,
+    required: [true, "Description is required"],
+    minlength: [10, "Description must be at least 10 characters"]
+  }
+});
 
-const noteModel = mongoose.model("note",noteSchema)
+const noteModel = mongoose.model("note", noteSchema);
 
-
-module.exports = noteModel
+module.exports = noteModel;
